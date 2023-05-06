15c119bb43e2058a1ab8cbc95d62189a35a4844f
Staudiz <s.staudenmayer@outlook.com>
2023-05-06 10:46:54 +0200

    Added Functionality to skip exercises and got to previous exercises

--- Dashboard/src/views/Workout.vue
+++ Dashboard/src/views/Workout.vue
@@ -11,23 +11,28 @@
         </v-card>
     </div>
     <div class="d-flex justify-center mb-6">
-        <v-btn @click.stop="" class="ma-2 pa-2"><v-icon icon="mdi-skip-previous"></v-icon></v-btn>
-        <v-btn @click.stop="startTimer" class="ma-2 pa-2"><v-icon icon="mdi-play"></v-icon></v-btn>
-        <v-btn @click.stop="pauseTimer" class="ma-2 pa-2"><v-icon icon="mdi-pause"></v-icon></v-btn>
-        <v-btn @click.stop="clearTimer" class="ma-2 pa-2"><v-icon icon="mdi-replay"></v-icon></v-btn>
-        <v-btn @click.stop="" class="ma-2 pa-2"><v-icon icon="mdi-skip-next"></v-icon></v-btn>
+        <v-btn v-if="timerActive || currTime != 0" @click.stop="prevExercise" class="ma-2 pa-2"><v-icon icon="mdi-skip-previous"></v-icon></v-btn>
+        <v-btn v-if="!timerActive" @click.stop="startTimer" class="ma-2 pa-2"><v-icon icon="mdi-play"></v-icon></v-btn>
+        <v-btn v-if="timerActive" @click.stop="pauseTimer" class="ma-2 pa-2"><v-icon icon="mdi-pause"></v-icon></v-btn>
+        <v-btn v-if="timerActive || currTime != 0" @click.stop="clearTimer" class="ma-2 pa-2"><v-icon icon="mdi-replay"></v-icon></v-btn>
+        <v-btn v-if="timerActive || currTime != 0" @click.stop="nextExercise" class="ma-2 pa-2"><v-icon icon="mdi-skip-next"></v-icon></v-btn>
 
     </div>
 </template>
 
 <script lang="ts">
+import { start } from 'repl';
+
 export default {
     data() {
         return {
             workouts: {},
             workoutName: "",
-            workoutSet: [],
+            workoutRoutine: [],
+            workoutExercise: 0,
+            workoutLength: 0,
             timer: '',
+            timerActive: false,
             setTime: 45,
             currTime: 0,
             percent: 0,
@@ -38,24 +43,29 @@ export default {
     },
     async mounted() {
         await this.getWorkouts();
-        console.log(this.workoutSet);
 
 
     },
     methods: {
         async getWorkouts() {
             this.workouts = await this.$axios.get('/workouts');
-            this.workoutSet = await this.workouts.data[0].sequence;
-            this.workoutName = await this.workouts.data[0].name;
+            this.workoutRoutin = await this.workouts.data[0].sequence;
+            this.workoutName = await this.workoutRoutin[this.workoutExercise].name;
+            this.workoutLength = await this.workouts.data[0].sequence.length;
         },
         timerFunction() {
             if (this.currTime === this.setTime * 100) {
                 this.currTime = 0;
                 this.percent = 0;
+                if(this.workoutExercise === this.workoutLength-1)
+                    return this.clearTimer();
+                this.workoutExercise++;
+                this.workoutName = this.workoutRoutin[this.workoutExercise].name;
             }
             if (this.currTime % this.setTime === 0)
                 this.percent++;
             this.currTime++;
+            this.timerActive=true;
 
         },
         startTimer() {
@@ -63,11 +73,32 @@ export default {
         },
         pauseTimer() {
             clearInterval(this.timer);
+            this.timerActive=false;
         },
         clearTimer(){
             this.pauseTimer();
             this.currTime = 0;
             this.percent = 0;
+        },
+        nextExercise(){
+            let timerActive = this.timerActive;
+            this.clearTimer();
+            if(this.workoutExercise !== this.workoutLength-1){
+                this.workoutExercise++;
+                this.workoutName = this.workoutRoutin[this.workoutExercise].name;
+                if(timerActive)
+                    this.startTimer();
+            }
+        },
+        prevExercise(){
+            let timerActive = this.timerActive;
+            this.clearTimer();
+            if(this.workoutExercise !== 0){
+                this.workoutExercise--;
+                this.workoutName = this.workoutRoutin[this.workoutExercise].name;
+                if(timerActive)
+                    this.startTimer();
+            }
         }
     }
 }
